de2b1268c087bae10e5c8e72ed7b498815990051
liso <lisowycliff@gmail.com>
2024-06-24 22:09:40 +0200

    create header component for the site

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import SignIn from './Pages/SignIn';
 import About from './Pages/About';
 import Profile from './Pages/Profile';
 import SignUp from './Pages/SignUp';
+import Header from './components/Header';
 
 
 export default function App() {
   return <BrowserRouter>
+ <Header/>
   <Routes>
    <Route path='/' element={<Home />} />
    <Route path= '/sign-in' element={<SignIn />} />
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,5 +3,6 @@ import React from 'react'
 export default function About() {
   return (
     <div>About</div>
+    
   )
 }
